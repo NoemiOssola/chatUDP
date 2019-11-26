4107a72ef7a40735bb9886a5acb4fee1b5fbb7ed
NoemiOssola <56298927+NoemiOssola@users.noreply.github.com>
2019-11-26 11:44:14 +0100

    Update Gui.java

--- client/Gui.java
+++ client/Gui.java
@@ -8,17 +8,24 @@ package client1;
 import java.awt.GridLayout;
 import java.awt.event.*;
 import java.io.IOException;
-import java.net.*;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.UnsupportedEncodingException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 /**
  *
  * @author pc15
  */
+
 public class Gui extends JFrame implements ActionListener {
+
     JPanel p;
     JLabel i;
     JTextField input;
@@ -27,11 +34,37 @@ public class Gui extends JFrame implements ActionListener {
     JButton invia;
     JButton inizia;
 
-    public Gui() {
-        this.setBounds(500,200,200,500);
+     DatagramSocket socket = new DatagramSocket();
+
+    //private static String passaggio = "";
+
+    public Gui() throws SocketException {
+       
+    
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                byte[] buffer = new byte[100];
+                DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+                for(;;){
+                try {
+                    socket.receive(dp);
+                    String received = new String(dp.getData(), 0, dp.getLength(), "ISO-8859-1");
+                    //passaggio = received;
+                    output.setText(received);
+
+                } catch (IOException ex) {
+                    Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }}
+        });
+
+        socket = new DatagramSocket();
+        this.setBounds(500, 200, 200, 500);
         this.setTitle("CHAT");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         this.p = new JPanel();
         p.setLayout(new GridLayout(4, 1));
         this.i = new JLabel("Inserisci messaggio");
@@ -43,71 +76,54 @@ public class Gui extends JFrame implements ActionListener {
         invia.addActionListener(this);
         this.inizia = new JButton("inizia");
         inizia.addActionListener(this);
-        
+
         this.add(p);
         p.add(i);
         p.add(input);
         p.add(inizia);
         p.add(invia);
         p.add(sjp);
-        
-        
+
         this.pack();
         this.setVisible(true);
     }
-    
-     String username;
+
+    String username;
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getActionCommand().equals("inizia")){
-             username = JOptionPane.showInputDialog(p,
-            "Inserisci user name", "Settaggio...", JOptionPane.ERROR_MESSAGE);
-         output.append("User: "  + username + "\n");
+        if (ae.getActionCommand().equals("inizia")) {
+            username = JOptionPane.showInputDialog(p,
+                    "Inserisci user name", "Settaggio...", JOptionPane.ERROR_MESSAGE);
+            output.append("User: " + username + "\n");
         }
-        if(ae.getActionCommand().equals("invia")){
+        if (ae.getActionCommand().equals("invia")) {
+            String i = input.getText();
+            output.setText(username + "> " + i);
             try {
-                String i = input.getText();
-                output.setText(username + "> " + i);
-                try {
-                    this.Invia(i);
-                } catch (UnknownHostException ex) {
-                    Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                this.Ricezione();
+                this.Invia(i);
+            } catch (UnknownHostException ex) {
+                Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException ex) {
                 Logger.getLogger(Gui.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-    
 
+    public void Invia(String messaggio) throws UnknownHostException, IOException {
 
-public void Invia(String messaggio) throws SocketException, UnknownHostException, IOException{
-    DatagramSocket socket = new DatagramSocket();
-    InetAddress ind = InetAddress.getByName("localhost");
-    int UDP_port = 1088;
-    byte[] buffer = new byte[100];
-    buffer = messaggio.getBytes();
-    DatagramPacket userDatagram = new DatagramPacket(buffer, buffer.length, ind, UDP_port);
-    socket.send(userDatagram);  
-    JOptionPane.showMessageDialog(p, "Ho inviato il messaggio", "Send", JOptionPane.ERROR_MESSAGE);
-}
+        InetAddress ind = InetAddress.getByName("localhost");
+        int UDP_port = 1088;
+        byte[] buffer = new byte[100];
+        buffer = messaggio.getBytes();
+        DatagramPacket userDatagram = new DatagramPacket(buffer, buffer.length, ind, UDP_port);
+        socket.send(userDatagram);
+        JOptionPane.showMessageDialog(p, "Ho inviato il messaggio", "Send", JOptionPane.ERROR_MESSAGE);
+    }
 
-public void Ricezione() throws SocketException, IOException{
-    DatagramSocket socket = new DatagramSocket(1088);
-    String utente = username;
-    byte[] buffer = new byte[100];
-    String received;
-    DatagramPacket serverDatagram = new DatagramPacket(buffer, buffer.length);
-            serverDatagram = new DatagramPacket(buffer, buffer.length);
-            while (!Thread.interrupted()){
-                socket.receive(serverDatagram);
-                received = new String(serverDatagram.getData(), 0, serverDatagram.getLength(), "ISO-8859-1");
-                output.setText(username + "> " + received);     
-}
-}
+    public static void main(String[] args) throws SocketException {
 
+        Gui g = new Gui();
+
+    }
 }
